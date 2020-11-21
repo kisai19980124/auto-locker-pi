d76fdbe651ba9a87f7b2efcbedc5a35dc1d843f6
kumagaimasahito <masahito.kumagai.t5@dc.tohoku.ac.jp>
2020-11-21 20:37:58 +0900

    Change the name of the module

--- src/auto-locker-pi/util/servo.py
+++ src/auto-locker-pi/util/servo.py
@@ -1,7 +1,7 @@
-import servo-angle
+import servoAngle
 
 def servo(gp_out, GPIO, angle):
     servo = GPIO.PWM(gp_out, 50)
     servo.start(0)
-    servo-angle(angle)
+    servoAngle(angle)
     servo.stop()
