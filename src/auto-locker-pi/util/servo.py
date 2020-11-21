15ffecc78af7cbeb1a15a0a7f2a1653769b28ac9
kumagaimasahito <masahito.kumagai.t5@dc.tohoku.ac.jp>
2020-11-21 20:42:32 +0900

    Modified to servoAngle(angle, servo)

--- src/auto-locker-pi/util/servo.py
+++ src/auto-locker-pi/util/servo.py
@@ -1,7 +1,7 @@
-import servoAngle
+from .servoAngle import servoAngle
 
 def servo(gp_out, GPIO, angle):
     servo = GPIO.PWM(gp_out, 50)
     servo.start(0)
-    servoAngle(angle)
+    servoAngle(angle, servo)
     servo.stop()
